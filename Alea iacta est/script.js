7ba485bde732c118f38bd7c8881e201a26ff56db
VesnaVuckovic <153770343+VesnaVuckovic@users.noreply.github.com>
2024-04-04 10:18:52 +0200

    2024/04/04_10:18
    Changed logic for number 6 (doesn't count to total score). Added "collect" option for both players.

--- Alea iacta est/script.js
+++ Alea iacta est/script.js
@@ -2,7 +2,8 @@ import Dice from './dice.js';
 
 const diceModule = Dice ();
 
-document.addEventListener ('DOMContentLoaded', function () { // Wait for DOM content to be loaded
+document.addEventListener ('DOMContentLoaded', function () {
+  // Wait for DOM content to be loaded
   const player1Panel = document.getElementById ('player1Panel'); // Get DOM elements
   const player2Panel = document.getElementById ('player2Panel');
   const rollDiceButton = document.getElementById ('rollDiceButton');
@@ -23,28 +24,33 @@ document.addEventListener ('DOMContentLoaded', function () { // Wait for DOM con
 
   updatePlayerStatus ();
 
-  rollDiceButton.addEventListener ('click', function () {// Event listener for roll dice button click
+  rollDiceButton.addEventListener ('click', function () {
+    // Event listener for roll dice button click
     if (!isGameWon && !isRolling) {
       isRolling = true;
       rollDiceButton.disabled = true;
       const roll = dice.rollDice ();
-      diceResult.style.display = 'block'; 
+      diceResult.style.display = 'block';
       if (roll === 1) {
         currentScore = 0;
         updateCurrentScore ();
         isRolling = false;
         rollDiceButton.disabled = false;
       } else {
-        currentScore += roll;
-        updateCurrentScore ();
+        if (roll !== 6) {
+          currentScore += roll;
+          updateCurrentScore ();
+        }
         if (roll === 6) {
           addCurrentScoreToTotal ();
           currentScore = 0;
           updateCurrentScore ();
           switchPlayers ();
         } else if (
-          totalScorePlayer1 + currentScore >= 101 || totalScorePlayer2 + currentScore >= 101
+          totalScorePlayer1 + currentScore >= 101 ||
+          totalScorePlayer2 + currentScore >= 101
         ) {
+          addCurrentScoreToTotal ();
           endGame ();
         } else {
           isRolling = false;
@@ -54,14 +60,28 @@ document.addEventListener ('DOMContentLoaded', function () { // Wait for DOM con
     }
   });
 
-  function updateCurrentScore () {// Function to update current score display
-    const currentPlayerScoreElement = currentPlayer === 1
-      ? document.getElementById ('player1CurrentScore')
-      : document.getElementById ('player2CurrentScore');
-    currentPlayerScoreElement.textContent = currentScore;
-  }
+  const collectButton1 = document.getElementById ('collectButton1');
+  const collectButton2 = document.getElementById ('collectButton2');
 
-  function updateTotalScore (player) {// Function to update total score display
+  collectButton1.addEventListener ('click', function () {//Event listener for collect current score for player1
+    if (!isGameWon && !isRolling && currentScore > 0 && currentPlayer === 1) {
+      addCurrentScoreToTotal ();
+      currentScore = 0;
+      updateCurrentScore ();
+      updateTotalScore (1);
+    }
+  });
+
+  collectButton2.addEventListener ('click', function () {//Event listener for collect current score for player2
+    if (!isGameWon && !isRolling && currentScore > 0 && currentPlayer === 2) {
+      addCurrentScoreToTotal ();
+      currentScore = 0;
+      updateCurrentScore ();
+      updateTotalScore (2);
+    }
+  });
+
+  function updateTotalScore (player) {//Function to update total score display
     const totalScoreElement = player === 1
       ? document.getElementById ('player1TotalScore')
       : document.getElementById ('player2TotalScore');
@@ -69,7 +89,14 @@ document.addEventListener ('DOMContentLoaded', function () { // Wait for DOM con
     totalScoreElement.textContent = totalScore;
   }
 
-  function addCurrentScoreToTotal () {// Function to add current score to total score
+  function updateCurrentScore () { //Function to update current score display
+    const currentPlayerScoreElement = currentPlayer === 1
+      ? document.getElementById ('player1CurrentScore')
+      : document.getElementById ('player2CurrentScore');
+    currentPlayerScoreElement.textContent = currentScore;
+  }
+
+  function addCurrentScoreToTotal () {// Function to add current score to total score    
     if (currentPlayer === 1) {
       totalScorePlayer1 += currentScore;
       updateTotalScore (1);
@@ -79,27 +106,32 @@ document.addEventListener ('DOMContentLoaded', function () { // Wait for DOM con
     }
   }
 
-  function switchPlayers () {// Function to switch players
-    currentPlayer = currentPlayer === 1 ? 2 : 1;
+  function switchPlayers () {// Function to switch players    
+    if (currentPlayer === 1) {
+      currentPlayer = 2;
+    } else {
+      currentPlayer = 1;
+    }
     updatePlayerStatus ();
     isRolling = false;
     rollDiceButton.disabled = false;
   }
 
-  function endGame() {// Function to end the game
-  if (totalScorePlayer1 >= 101 || totalScorePlayer2 >= 101) {
-    isGameWon = true;
-    rollDiceButton.disabled = true; 
-    updatePlayerStatus(); 
-  } else {
-    
-    isRolling = false;
-    rollDiceButton.disabled = false; 
-    updatePlayerStatus(); 
+  function endGame () {
+    // Function to end the game
+    if (totalScorePlayer1 >= 101 || totalScorePlayer2 >= 101) {
+      isGameWon = true;
+      rollDiceButton.disabled = true;
+      updatePlayerStatus ();
+    } else {
+      isRolling = false;
+      rollDiceButton.disabled = false;
+      updatePlayerStatus ();
+    }
   }
-}
 
-  function updatePlayerStatus () {// Function to update player status display
+  function updatePlayerStatus () {
+    // Function to update player status display
     const player1Status = document.getElementById ('player1Status');
     const player2Status = document.getElementById ('player2Status');
     if (isGameWon) {
