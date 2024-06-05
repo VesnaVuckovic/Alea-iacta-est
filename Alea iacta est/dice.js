b26db30ad876050b2e052618206cc5cb1dd13766
VesnaVuckovic <153770343+VesnaVuckovic@users.noreply.github.com>
2024-06-05 21:45:43 +0200

    2024/06/05_21:45
    All processes are functional.

--- Alea iacta est/dice.js
+++ Alea iacta est/dice.js
@@ -1,24 +1,25 @@
-const Dice = () => {
-  function rollDice () {
-    const dice = document.querySelector ('.die-list');
+export function Dice() {
+  function rollDice() {
+    const dice = document.querySelector('.die-list');
     if (dice) {
-      toggleClasses (dice);
-      const roll = rollDie ();
+      toggleClasses(dice);
+      const roll = rollDie();
       dice.dataset.roll = roll;
       return roll;
     } else {
-      console.error ('Dice element not found.');
+      console.error('Dice element not found.');
       return null;
     }
   }
-  function toggleClasses (die) {
-    die.classList.toggle ('odd-roll');
-    die.classList.toggle ('even-roll');
-  }
-  function rollDie () {
-    return Math.floor (Math.random () * 6) + 1;
-  }
-  return {rollDice};
-};
 
-export default Dice;
+  function toggleClasses(die) {
+    die.classList.toggle('odd-roll');
+    die.classList.toggle('even-roll');
+  }
+
+  function rollDie() {
+    return Math.floor(Math.random() * 6) + 1;
+  }
+
+  return { rollDice };
+}
